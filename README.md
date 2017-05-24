77b2d7c03e543b2424666843775505481be8d224
Jochen Klar <jklar@aip.de>
2017-05-24 09:59:50 +0200

    rename project again

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-django-client-test-mixins
-=========================
+django-test-generator
+=====================
 
 A set of mixins to automatically generate tests for generic Django views and DRF Viewsets.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = '0.1.2'
 
 setup(
-    name='django-client-test-mixins',
+    name='django-test-generator',
     packages=find_packages(),
     license=u'Apache License (2.0)',
     version=version,
@@ -12,8 +12,8 @@ setup(
     author_email='jklar@aip.de',
     maintainer=u'AIP E-Science',
     maintainer_email=u'escience@aip.de',
-    url='https://github.com/aipescience/django-client-test-mixins',
-    download_url='https://github.com/aipescience/django-client-test-mixins/archive/%s.tar.gz' % version,
+    url='https://github.com/aipescience/django-test-generator',
+    download_url='https://github.com/aipescience/django-test-generator/archive/%s.tar.gz' % version,
     keywords=['testing', 'django'],
     classifiers=[],
 )
