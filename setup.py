07ceaea6e35c223ac1b7acedede3d27ca819cbf7
Jochen Klar <jklar@aip.de>
2017-05-24 10:23:27 +0200

    update setup files

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-version = 'v0.1.2'
+version = '0.1.0'
 
 setup(
     name='django-test-generator',
-    packages=find_packages(),
-    license=u'Apache License (2.0)',
     version=version,
     description='A set of mixins to automatically generate test for generic Django views and DRF Viewsets.',
+    url='https://github.com/aipescience/django-test-generator',
     author='Jochen Klar',
     author_email='jklar@aip.de',
     maintainer=u'AIP E-Science',
     maintainer_email=u'escience@aip.de',
-    url='https://github.com/aipescience/django-test-generator',
+    license=u'Apache License (2.0)',
+    packages=['test_generator'],
     download_url='https://github.com/aipescience/django-test-generator/archive/%s.tar.gz' % version,
     keywords=['testing', 'django'],
     classifiers=[],
